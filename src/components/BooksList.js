77aee54868b7ceedcb733c6abf468d2142ad2889
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-07-26 12:32:21 +0200

    api endpoint  copied

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,7 +1,10 @@
  import React from "react";
 
+ import axios from "axios";
+
  const BooksList () => {
 
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyB_1-_2uklrOMSr49BIDIvBvhcPDkhyHJE")
     
  }
 
