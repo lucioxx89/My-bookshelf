139b8d05b3698fee610a624e524518715b926f4e
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-07-27 19:28:08 +0200

    problem while rendering books on input search

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,54 +2,58 @@ import React, { useState, useEffect } from "react";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = `https://www.googleapis.com/books/v1/volumes?q=jhon&key=${API_KEY}&maxResults=40`;
-
 const BooksList = () => {
   const [booksList, setBooksList] = useState([]);
 
-  // const [searchBook, setSearchBook] = useState("");
+  const [searchBook, setSearchBook] = useState("");
+
+  // option 2, before fetch
+  // useEffect(() => {
+  //   getBooksList();
+  // }, []);
 
   //function fetch to add then to useEffect, cleaner
-  const getBooksList = async () => {
-    const response = await fetch(
-      // `https://www.googleapis.com/books/v1/volumes?q=rowling&key=AIzaSyDDRDhXJtVOYtKlI-azc7_3321mnAaMDJo&maxResults=40`
-      API_URL
-      // "http://localhost:3000/books.json"
-    );
-    const data = await response.json();
 
-    setBooksList(data.items);
-    console.log("array", data.items);
+  const onSubmitHandler = (event) => {
+    event.preventdefault();
+
+    const getBooksList = async () => {
+      const response = await fetch(
+        // `https://www.googleapis.com/books/v1/volumes?q=rowling&key=AIzaSyDDRDhXJtVOYtKlI-azc7_3321mnAaMDJo&maxResults=40`
+        `https://www.googleapis.com/books/v1/volumes?q=rambo&key=${API_KEY}&maxResults=40`
+        // "http://localhost:3000/books.json"
+      );
+      const data = await response.json();
+
+      setBooksList(data.items);
+      console.log("array", data.items);
+    };
   };
 
-  useEffect(() => {
-    getBooksList();
-  }, []);
-
-  // const onSubmitHandler = (event) => {
-  //   event.preventdefault();
+  //option 1
+  // useEffect(() => {
   //   getBooksList();
-  // };
+  // }, []);
 
-  // const onChangeHandler = (event) => {
-  //   setSearchBook(event.target.value);
-  //   console.log("target", event.target.value);
-  // };
+  const onChangeHandler = (event) => {
+    setSearchBook(event.target.value);
+    console.log("target", event.target.value);
+  };
 
   return (
     <div>
       <h1> My bookshelf</h1>
 
-      {/* <form onSubmit={onSubmitHandler}>
+      <form onSubmit={onSubmitHandler}>
         <input
           type="text"
           name="book"
           placeholder="Search your book"
-          // value={searchBook}
+          value={searchBook}
           onChange={onChangeHandler}
         />
         <button type="submit">Search</button>
-      </form> */}
+      </form>
 
       {booksList.map((item, index) => {
         let bookCover =
